aaaa2d7cf61a69ae183a87122faddbb91e85f23e
Andrew Ilchenko <a4etko@gmail.com>
2018-03-07 14:34:44 +0200

    add activity_post

--- app/src/main/java/com/app/ui/post/PostAdapter.kt
+++ app/src/main/java/com/app/ui/post/PostAdapter.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.app.R
+import com.app.databinding.ItemPostBinding
 import com.app.model.Post
 
 /**
@@ -25,7 +26,25 @@ class PostAdapter(private val context: Context) : RecyclerView.Adapter<PostAdapt
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         val layoutInflater = LayoutInflater.from(context)
-        val binding: ItemPostBinding = DataBindingUtil.inflate(layoutInflater, R.layout.item_post)
+        val binding: ItemPostBinding = DataBindingUtil.inflate(layoutInflater, R.layout.item_post, parent, false)
+        return PostViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder?, position: Int) {
+        holder?.bind(posts[position])
+    }
+
+    /**
+     * Updates the list of posts of the adapter
+     * @param posts the new list of posts of the adapter
+     */
+    fun updatePosts(posts: List<Post>) {
+        this.posts = posts
+        notifyDataSetChanged()
     }
 
     /**
@@ -33,6 +52,7 @@ class PostAdapter(private val context: Context) : RecyclerView.Adapter<PostAdapt
      * @property binding the DataBinging object for Post item
      */
     class PostViewHolder(private val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root) {
+
         /**
          * Binds a post into the view
          */
