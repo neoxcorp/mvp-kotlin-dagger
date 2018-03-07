7be5433dc7b9f346abdcf1146cf60e11975ff1ba
Andrew Ilchenko <a4etko@gmail.com>
2018-03-07 11:47:16 +0200

    changed package

--- app/src/main/java/com/app/kotlindagger/utils/constants.kt
+++
@@ -1,8 +0,0 @@
-package com.app.kotlindagger.utils
-
-/**
- * Created by andrew on 3/6/2018
- */
-
-/** The base URL of the API */
-const val BASE_URL: String = "https://jsonplaceholder.typicode.com"
